a260ab8dc6f61bb779f2b9e5e31a4eec3b8b1513
Darkwesy <thiagopazba@gmail.com>
2024-03-18 07:05:09 -0300

    Adicionar arquivos na galeria e fazer ajuster na responsividade.

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,6 +1,46 @@
-import './Gallery_style.css'
+import { useState, useEffect } from 'react';
+import './Gallery_style.css';
 
 function Gallery() {
+  const images = [
+    { url: "/products_assets/product_1.jpg" },
+    { url: "/products_assets/product_2.jpg" },
+    { url: "/products_assets/product_3.jpg" },
+    { url: "/products_assets/product_4.jpg" },
+    { url: "/products_assets/product_5.jpg" }
+  ];
+
+  const [bigCardIndex, setBigCardIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setBigCardIndex(prevIndex => (prevIndex + 1) % images.length);
+    }, 3000);
+
+    return () => clearInterval(interval);
+  }, [images.length]);
+
+  return (
+    <div className="Gallery__Wrapper">
+      <div className="Gallery__Wrapper__BigCard">
+        <img src={images[bigCardIndex].url} alt="" className="Gallery__Wrapper__BigCard__Image" />
+      </div>
+      <div className="Gallery__Wrapper__Card">
+        {images.map((image, index) => (
+          <div
+            key={index}
+            className="Gallery__Wrapper__Card__Item"
+          >
+            <img
+              src={image.url}
+              alt=""
+              className="Gallery__Wrapper__Card__Image"
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default Gallery
+export default Gallery;
