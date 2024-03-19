db880725a181c8f7cc4f6bce1059868d79f9eaac
Darkwesy <thiagopazba@gmail.com>
2024-03-19 10:28:45 -0300

    Adicionar novos arquivos estaticos e ajustar tamanho do card menor da galeria

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -7,7 +7,22 @@ function Gallery() {
     { url: "/products_assets/product_2.jpg" },
     { url: "/products_assets/product_3.jpg" },
     { url: "/products_assets/product_4.jpg" },
-    { url: "/products_assets/product_5.jpg" }
+    { url: "/products_assets/product_5.jpg" },
+    { url: "/products_assets/product_6.jpg" },
+    { url: "/products_assets/product_7.jpg" },
+    { url: "/products_assets/product_8.jpg" },
+    { url: "/products_assets/product_9.jpg" },
+    { url: "/products_assets/product_10.jpg" },
+    { url: "/products_assets/product_11.jpg" },
+    { url: "/products_assets/product_12.jpg" },
+    { url: "/products_assets/product_13.jpg" },
+    { url: "/products_assets/product_14.jpg" },
+    { url: "/products_assets/product_15.jpg" },
+    { url: "/products_assets/product_16.jpg" },
+    { url: "/products_assets/product_17.jpg" },
+    { url: "/products_assets/product_18.jpg" },
+    { url: "/products_assets/product_19.jpg" },
+    { url: "/products_assets/product_20.jpg" }
   ];
 
   const [bigCardIndex, setBigCardIndex] = useState(0);
--- src/components/Slide_Products/Slide_Products.jsx
+++ src/components/Slide_Products/Slide_Products.jsx
@@ -4,22 +4,16 @@ import { MdArrowBackIos, MdArrowForwardIos } from "react-icons/md";
 
 function Slider() {
   const images = [
-    {
-      url: "/products_assets/product_1.jpg"
-    },
-    {
-      url: "/products_assets/product_2.jpg"
-    },
-    {
-      url: "/products_assets/product_3.jpg"
-    },
-    {
-      url: "/products_assets/product_4.jpg"
-    },
-    {
-      url: "/products_assets/product_5.jpg"
-    }
+    { url: "/products_assets/product_1.jpg" },
+    { url: "/products_assets/product_2.jpg" },
+    { url: "/products_assets/product_3.jpg" },
+    { url: "/products_assets/product_4.jpg" },
+    { url: "/products_assets/product_5.jpg" },
+    { url: "/products_assets/product_6.jpg" },
+    { url: "/products_assets/product_7.jpg" },
+    { url: "/products_assets/product_8.jpg" }
   ];
+
   
   const [actualIndex, setIndex] = useState(0);
 
